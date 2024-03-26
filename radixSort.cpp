cde3d900affb734e589747bffb3f79219cde0223
JohnAmore <danben3@hotmail.com>
2024-03-26 06:01:05 -0400

    Fix .cpp & .h issue

--- radixSort.cpp
+++ radixSort.cpp
@@ -1,19 +1,37 @@
-#pragma once
-
 #include <string>
-#inclide <array>
+#include <array>
+#include "radixSort.h"
 
 using namespace std;
 
-class radixSort {
-public:
-  radixSort();
-  countSort(string A[], int n, int pos);
-  sort(string A[], int n, int d);
-  ~radixSort();
+radixSort::radixSort()
+{
+
+}
+
+radixSort::countSort(string A[], int n, int pos)
+{
+  int range = 26; // Range of letters. We know that there are 26 letters in the English Alphabet.
+  int count[range]; // Instantiating array B(count)
+  
+  //Count each occurance of each letter
+  for (int i = 0; i < n; ++i) 
+  }
+        char ch = A[i][pos];
+        count[ch]++;
+  }
+  //Convert occurrences to the index positions
+  for (int i = 1; i < range; ++i) 
+  {
+        count[i] += count[i - 1];
+  }
 
   
-};
+}
 
-
-
+radixSort::sort(string A[], int n, int d)
+{
+    for (int i = d - 1; i >= 0; --i) {
+        radixSort::countSort(A, n, i);
+    }
+}
